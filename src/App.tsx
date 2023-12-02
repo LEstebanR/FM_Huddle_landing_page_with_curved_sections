dbed2efe2c5c98b495d625f3f4cec5e410756218
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-12-01 22:55:34 -0500

    add section one

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Title from "./components/sections/title";
+import SectionOne from "./components/sections/section-one";
 
 function App() {
   return (
     <div className="w-screen flex flex-col gap-14 items-center px-10 md:px-20 font-Body">
       <Header />
       <Title />
+      <SectionOne />
       <Footer />
     </div>
   );
---
+++ src/components/sections/section-one.tsx
@@ -0,0 +1,15 @@
+const SectionOne = () => {
+  return (
+    <section className="bg-top-mobile w-full">
+      <p>Image</p>
+      <p>Grow together</p>
+      <p>
+        Generate meaningful discussions with your audience and build a strong,
+        loyal community. Think of the insightful conversations you miss out on
+        with a feedback form.
+      </p>
+    </section>
+  );
+};
+
+export default SectionOne;
